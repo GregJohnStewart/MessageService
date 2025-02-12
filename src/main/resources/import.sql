094d24c020515707b514e211aef4b05bb44e1a77
shadowklr <csereno42@gmail.com>
2025-02-12 09:49:01 -0500

    more changes for model

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,11 +4,11 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
-insert into PriorityMessage (id, subject, priority, content, postingUser, startDate, endDate, createdAt, lastUpdated) 
-  values (gen_random_uuid(), 'Message 1', '1', 'Message 1 content', 'import', '2025-01-05', '2025-01-05', '2025-01-05 12:25:00', '2025-01-06 14:24:00');
-insert into PriorityMessage (id, subject, priority, content, postingUser, startDate, endDate, createdAt, lastUpdated) 
-  values (gen_random_uuid(), 'Message 2', '2', 'Message 2 content', 'import', '2025-01-06', '2025-01-08', '2025-01-05 12:35:00', '2025-01-06 14:34:00');
-insert into PriorityMessage (id, subject, priority, content, postingUser, startDate, endDate, createdAt, lastUpdated) 
-  values (gen_random_uuid(), 'Message 3', '3', 'Message 3 content', 'import', '2025-01-07', '2025-01-09', '2025-01-05 12:45:00', '2025-01-06 14:44:00');
-insert into PriorityMessage (id, subject, priority, content, postingUser, startDate, endDate, createdAt, lastUpdated) 
-  values (gen_random_uuid(), 'Message 4', '4', 'Message 4 content', 'import', '2025-01-08', '2025-01-10', '2025-01-05 12:55:00', '2025-01-06 14:54:00');
+insert into PriorityMessage (id, subject, priority, content, startDate, endDate, createdBy, createdAt, lastUpdatedBy, lastUpdated) 
+  values (gen_random_uuid(), 'Message 1', '1', 'Message 1 content', '2025-01-05', '2025-01-05', 'import', '2025-01-05 12:25:00', 'import', '2025-01-06 14:24:00');
+insert into PriorityMessage (id, subject, priority, content, startDate, endDate, createdBy, createdAt, lastUpdatedBy, lastUpdated) 
+  values (gen_random_uuid(), 'Message 2', '2', 'Message 2 content', '2025-01-06', '2025-01-08', 'import', '2025-01-05 12:35:00', 'import', '2025-01-06 14:34:00');
+insert into PriorityMessage (id, subject, priority, content, startDate, endDate, createdBy, createdAt, lastUpdatedBy, lastUpdated) 
+  values (gen_random_uuid(), 'Message 3', '3', 'Message 3 content', '2025-01-07', '2025-01-09', 'import', '2025-01-05 12:45:00', 'import', '2025-01-06 14:44:00');
+insert into PriorityMessage (id, subject, priority, content, startDate, endDate, createdBy, createdAt, lastUpdatedBy, lastUpdated) 
+  values (gen_random_uuid(), 'Message 4', '4', 'Message 4 content', '2025-01-08', '2025-01-10', 'import', '2025-01-05 12:55:00', 'import', '2025-01-06 14:54:00');
