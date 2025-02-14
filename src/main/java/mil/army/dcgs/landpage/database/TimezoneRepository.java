c838f2d76b2c87187e2dc5a4fdd66c1b0a5a29c4
shadowklr <csereno42@gmail.com>
2025-02-14 16:05:50 -0500

    sorted timezones

--- src/main/java/mil/army/dcgs/landpage/database/TimezoneRepository.java
+++ src/main/java/mil/army/dcgs/landpage/database/TimezoneRepository.java
@@ -8,6 +8,9 @@ import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -47,8 +50,44 @@ public class TimezoneRepository implements PanacheRepository<Timezone> {
     }
     
     public List<String> getAllZoneIds() {
-        List<String> list = new ArrayList<>(ZoneId.getAvailableZoneIds());
+//        List<String> list = new ArrayList<>(ZoneId.getAvailableZoneIds());
+        List<String> list = getSortedTimeZones();
+
+
         return list;
+    }
+
+    /**
+     * Instead of using the complete (and unsorted) list of timezones...do some magic
+     * and sort them to more easily find the timezone name.  Also put the common ones 
+     * "GMT", "US/Eastern", "US/Central", "US/Pacific" at the beginning to more easily find them.
+     * 
+     * @return list of sorted timezones
+     */
+    public static List<String> getSortedTimeZones() {
+        // Retrieve all available zone IDs
+        List<String> zoneIds = new ArrayList<>(ZoneId.getAvailableZoneIds());
+
+        // Create a list and sort it alphabetically
+        List<String> sortedZones = new ArrayList<>(zoneIds);
+
+        // Remove all zones that start with "Etc/"
+        sortedZones.removeIf(zone -> zone.startsWith("Etc/"));
+
+        // Sort the remaining zones alphabetically
+        Collections.sort(sortedZones);
+
+        // Define the time zones to prioritize
+        List<String> prioritizedZones = Arrays.asList("GMT", "US/Eastern", "US/Central", "US/Pacific");
+
+        // Remove prioritized zones from the sorted list (if present)
+        sortedZones.removeAll(prioritizedZones);
+
+        // Add the prioritized zones to the beginning
+        List<String> finalList = new ArrayList<>(prioritizedZones);
+        finalList.addAll(sortedZones);
+
+        return finalList;
     }
     
     public float getSingleOffset(String identifier) {
